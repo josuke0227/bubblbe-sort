c503c08b88f7fcd1bbf88fdd912eec0b042f61bd
Yosuke Motosugi <y.motosugi0227@gmail.com>
2023-09-18 20:51:33 +1000

    Initial Commit

--- app.rb
+++ app.rb
@@ -1,2 +1,21 @@
-input = [4, 3, 2, 1]
 
+def bubble_sort(numbers)
+    return numbers if numbers.empty?
+
+    (numbers.length - 1).times do
+        for i in 1...numbers.length do
+            left = numbers[i - 1]
+            right = numbers[i]
+            if left > right
+                temp = right
+                numbers[i] = left
+                numbers[i - 1] = temp 
+            end
+        end
+    end
+    numbers
+end
+    
+input = [4, 3, 2, 1]
+# input = []
+p bubble_sort(input.shuffle)
